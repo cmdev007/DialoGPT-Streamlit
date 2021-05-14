838428f919b681bfb68e16e4cf440fc29c126c0e
Preet Patel <preet.makz@gmail.com>
2021-05-14 12:48:58 +0530

    chatSUI: Added context and user profiling

--- chatSUI.py
+++ chatSUI.py
@@ -1,10 +1,11 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-# import torch
+import torch
+import pickle, os
 import streamlit as st
 from load_css import local_css
 
 st.markdown(f"<div style='font-size: 50px;'><center><b>Talk with Jarvis</b></center></div>", unsafe_allow_html=True)
-st.markdown("---")
+st.markdown(f"<div style='font-size: 12px;'><center>By <a href='https://github.com/cmdev007/'><span class='highlight green'><span class='bold'>cMDev007</span></span></a></center></div>", unsafe_allow_html=True)
 
 local_css("style.css")
 
@@ -17,38 +18,91 @@ def Loader():
     model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
     return tokenizer, model
 
+@st.cache(allow_output_mutation=True)
+def Namer(name):
+    a = name
+    return a
+
 tokenizer, model = Loader()
-# Let's chat for 5 lines
-f = open("step.txt")
-step = int(f.read())
-f.close()
 
-# encode the new user input, add the eos_token and return a tensor in Pytorch
-hInput = st.text_input("Type:")
-if st.button("OK"):
-    HI = f"<div><span class='highlight blue'><span class='bold'>You: </span>{hInput}</span></div>"
-    st.markdown(HI, unsafe_allow_html=True)
-    new_user_input_ids = tokenizer.encode(hInput + tokenizer.eos_token, return_tensors='pt')
+cont0 = st.beta_container()
+col03, col01, col02, col04 = cont0.beta_columns([0.15, 1, 0.15, 0.15])
 
-    # append the new user input tokens to the chat history
-    # bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-    bot_input_ids = new_user_input_ids
+nInput = col01.text_input("Your name please:")
+nInput = nInput.lower().replace(" ","")
+context = col02.selectbox("context",('2', '3', '4', '5'))
+context = int(context)-1
 
-    # generated a response while limiting the total chat history to 1000 tokens,
-    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+if nInput != "":
+    if nInput not in os.listdir():
+        os.system(f"mkdir {nInput}")
+    st.markdown("---")
 
-    # pretty print last ouput tokens from bot
-    AIOut = ("{}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+    try:
+        f = open(f"{nInput}/step.txt")
+        step = int(f.read())
+        f.close()
+    except:
+        f = open(f"{nInput}/step.txt", "w")
+        f.write("0")
+        f.close()
 
-    AI = f"<div><span class='highlight red'><span class='bold'>AI: </span>{AIOut}</span></div>"
-    st.write("")
-    st.markdown(AI, unsafe_allow_html=True)
+    cont1 = st.beta_container()
+    col3, col1, col2, col4 = cont1.beta_columns([0.15, 1, 0.125, 0.15])
+    # col2.button("OK")
+    # uName = Namer(nInput)
+    # st.info(f"Hello {uName}")
+    col2.write("")
+    col2.write("")
 
-    if step == 4:
-        step = 0
-    else:
-        step+=1
+    # encode the new user input, add the eos_token and return a tensor in Pytorch
+    hInput = col1.text_input("Type:")
+    if col2.button("Send"):
+        HI = f"<div><span class='highlight blue'><span class='bold'>You: </span>{hInput}</span></div>"
+        col1.markdown(HI, unsafe_allow_html=True)
+        print(f"{nInput} : {hInput}")
+        new_user_input_ids = tokenizer.encode(hInput + tokenizer.eos_token, return_tensors='pt')
 
-    f = open("step.txt","w")
-    f.write(str(step))
-    f.close()
+        # append the new user input tokens to the chat history
+        try:
+            g = open(f"{nInput}/chat_history_ids", "rb")
+            chat_history_ids = pickle.load(g)
+            g.close()
+            bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids],
+                                      dim=-1) if step > 0 else new_user_input_ids
+        except:
+            step = 0
+            bot_input_ids = new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens,
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        g = open(f"{nInput}/chat_history_ids", "wb")
+        pickle.dump(chat_history_ids, g)
+        g.close()
+        # pretty print last ouput tokens from bot
+        AIOut = (
+            "{}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+
+        AI = f"<div><span class='highlight red'><span class='bold'>AI: </span>{AIOut}</span></div>"
+        col1.write("")
+        col1.markdown(AI, unsafe_allow_html=True)
+        print(f"AI : {AIOut}")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        if step >= context:
+            step = 0
+        else:
+            step += 1
+
+        f = open(f"{nInput}/step.txt", "w")
+        f.write(str(step))
+        f.close()
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
